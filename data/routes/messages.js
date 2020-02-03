e7c5d8fc5411d3be02d14c3c81e5d011b33a0326
Gabby <gabriellapelton@gmail.com>
2020-02-03 03:10:21 -0600

    use ethereal for email

--- data/routes/messages.js
+++ data/routes/messages.js
@@ -2,7 +2,7 @@ const express = require("express");
 const uuidV4 = require("uuid/v4");
 const Message = require("../models/message");
 const User = require("../models/user");
-const { emailTransporter } = require("../../server");
+const { sendMail } = require("../../server");
 
 const router = express.Router();
 
@@ -13,7 +13,7 @@ router
       const newMessage = new Message({ content: message, token });
       const savedMessage = await newMessage.save();
       res.status(200).json({ savedMessage });
-      await emailTransporter.sendMail({
+      await sendMail({
         to: process.env.EMAIL_RECIPIENT,
         subject: "New message from gabriellapelton.com",
         text: `user: ${token}\n\nmessage: ${message}`
@@ -27,7 +27,7 @@ router
     const newUser = new User({ token });
     res.status(200).json({ token });
     await newUser.save();
-    await emailTransporter.sendMail({
+    await sendMail({
       to: process.env.EMAIL_RECIPIENT,
       subject: "New user token created",
       text: "A new user has visited the site"
@@ -38,7 +38,7 @@ router
       const { token } = req.query;
       const messages = await Message.find({ token });
       res.status(200).json({ messages });
-      await emailTransporter.sendMail({
+      await sendMail({
         to: process.env.EMAIL_RECIPIENT,
         subject: "A visitor returns",
         text: `User ${token} has returned`
--- server.js
+++ server.js
@@ -5,50 +5,61 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const nodemailer = require("nodemailer");
 
-const emailTransporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
-  }
-});
+async function sendMail({to, subject, text}) {
+  let testAccount = await nodemailer.createTestAccount();
 
-const messageRouter = require("./data/routes/messages");
-
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-h73bz.mongodb.net/test?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => {
-    console.log("~~~ connected to db ~~~");
-  })
-  .catch(err => {
-    console.log(err);
+  const emailTransporter = nodemailer.createTransport({
+    host: "smtp.ethereal.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass
+    }
   });
 
-const server = express();
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
-server.use(
-  morgan("method :url :status :res[content-length] - :response-time ms")
-);
-server.use("/messages", messageRouter);
-
-server.get("/", async (req, res) => {
-  res.status(200).json("Hello squirrel");
   await emailTransporter.sendMail({
-    to: process.env.EMAIL_RECIPIENT,
-    subject: "Direct API accesss",
-    text: "Someone is hitting up your API directly"
+    to,
+    subject,
+    text
   });
-});
+}
 
-server.listen(process.env.PORT, () => {
-  console.log(`LISTENING ON PORT ${process.env.PORT}`);
-});
+  const messageRouter = require("./data/routes/messages");
 
-module.exports = {emailTransporter}
+  mongoose
+    .connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-h73bz.mongodb.net/test?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => {
+      console.log("~~~ connected to db ~~~");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  const server = express();
+  server.use(helmet());
+  server.use(express.json());
+  server.use(cors());
+  server.use(
+    morgan("method :url :status :res[content-length] - :response-time ms")
+  );
+  server.use("/messages", messageRouter);
+
+  server.get("/", async (req, res) => {
+    res.status(200).json("Hello squirrel");
+    await sendMail({
+      to: process.env.EMAIL_RECIPIENT,
+      subject: "Direct API accesss",
+      text: "Someone is hitting up your API directly"
+    });
+  });
+
+  server.listen(process.env.PORT, () => {
+    console.log(`LISTENING ON PORT ${process.env.PORT}`);
+  });
+
+module.exports = { sendMail }
+
